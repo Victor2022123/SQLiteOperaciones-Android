5cf985de89db5fbb60e4091ac456777adac28e75
victor <vise.c2@gmail.com>
2023-10-25 22:50:39 -0300

    Implementacion 3 de Layout, Clase Mensaje y Clase DbAdapter.

---
+++ app/src/main/java/com/victor/sqliteoperaciones/MainActivity.java
@@ -0,0 +1,44 @@
+package com.victor.sqliteoperaciones;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    EditText usuario, password;
+    DbAdapter helper;
+    protected void onCreate(Bundle savedInstanceState){
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        usuario = (EditText) findViewById(R.id.usuarioNombre);
+        password= (EditText) findViewById(R.id.usuarioPassword);
+
+        helper= new DbAdapter(this);
+    }
+    public void agregarUsuario(View view){
+
+        String datoUsuario =usuario.getText().toString();
+        String datoPassword =password.getText().toString();
+        if (datoUsuario.isEmpty() || datoPassword.isEmpty())
+        {
+            Mensaje.aviso(this, "Ingrese Tanto el nombre como la Contrasena");
+        }else{
+            long  resultado = helper.insertarDatos(datoUsuario,datoPassword);
+            if(resultado<=0){
+                Mensaje.aviso(this, "Insercion Fallida");
+                usuario.setText("");
+                password.setText("");
+
+            }else{
+                Mensaje.aviso(this, "Insercion Exitosa");
+                usuario.setText("");
+                password.setText("");
+
+            }
+        }
+    }
+
+}
